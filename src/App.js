feac51238b526397a37c08b9c6744c1d74bd491f
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2023-01-24 21:31:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="hero">
+        <header>
+          <img src={logo} className="logo" alt="logo" />
+          <div>
+            <a href=" ">Home</a>
+            <a href=" ">Landlord</a>
+            <a href=" ">Tenants</a>
+            <a href=" ">Contact Us</a>
+          </div>
+        </header>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
